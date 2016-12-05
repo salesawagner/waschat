b9423e484047eb094387300371799570fc020c59
Wagner Sales <salesawagner@gmail.com>
2016-12-05 15:51:17 +1100

    xcode commit

--- Pods/Target Support Files/Pods-waschat/Pods-waschat-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_waschat : NSObject
-@end
-@implementation PodsDummy_Pods_waschat
-@end
--- Pods/Target Support Files/Pods-waschatTests/Pods-waschatTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_waschatTests : NSObject
-@end
-@implementation PodsDummy_Pods_waschatTests
-@end
